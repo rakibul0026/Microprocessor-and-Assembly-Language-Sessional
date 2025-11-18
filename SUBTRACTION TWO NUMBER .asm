6cf434aa7c1d787be4138326f931f56405f07552
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-11-18 23:46:28 +0600

    Create SUBTRACTION TWO NUMBER .asm

---
+++ SUBTRACTION TWO NUMBER .asm
@@ -0,0 +1,39 @@
+.MODEL SMALL
+.STACK 100H
+.CODE
+
+MAIN PROC
+    MOV AH,1  ;INPUT FUNCTION
+    INT 21H
+    
+    MOV BL,AL  ;1ST INPUT
+    INT 21H
+    
+    MOV BH,AL    ;2ND INPUT
+  
+    
+
+    
+   SUB  BL,BH    ;SUM
+   
+    
+    
+    
+   MOV AH,2
+   MOV DL,10      ;NEWLINE
+   INT 21H
+   MOV DL,13 
+   INT 21H
+    
+   
+   MOV AH,2 
+   ADD BL,48
+   MOV DL,BL    ;print
+   INT 21H
+   
+    
+   EXIT:
+   MOV AH,4CH
+   INT 21H
+   MAIN ENDP
+END MAIN
