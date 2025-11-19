021a9f4b8bd08adc8e4fc9eee3e35c29c953ce4b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-11-19 20:09:45 +0600

    Create Uppercase to lowercase.asm

---
+++ Uppercase to lowercase.asm
@@ -0,0 +1,21 @@
+; Uppercase to lowercase
+.model small
+.stack 100h
+.code
+
+main proc
+    mov ah,1
+    int 21h
+    mov bl,al
+    
+    
+    add bl,32
+    
+    
+    mov ah,2
+    mov dl,bl 
+    int 21h
+    
+    exit:
+    main endp
+end main
