4f8bf5570f7d6ea900457e8ce078948a57a81517
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-11-20 20:43:50 +0600

    Create for_loop and jmp operation .asm

---
+++ for_loop and jmp operation .asm
@@ -0,0 +1,32 @@
+INCLUDE 'EMU8086.INC'
+.MODEL SMALL
+.STACK 100H
+.DATA
+.CODE
+
+MAIN PROC
+
+    ; INC BX = BX+1 ; BX++
+    ; DEC BX = BX-1 ; BX--
+
+    MOV BX, 0
+    MOV CX, 5
+
+    ; FOR(BX = 0 ; BX < CX ; BX++)
+
+STAT:
+    CMP BX, CX
+    JE LAST
+    PRINTN "HELLO WORLD"
+    INC BX
+    JNE STAT
+
+LAST:
+    PRINT "THE PROGRAMMING IS END"
+
+EXIT:
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
