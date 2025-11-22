06153f4f18c3ee68333b231a133b48503bf6bca4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-11-23 01:37:04 +0600

    Add factorial calculation in assembly language
    Implement factorial calculation using assembly language.

--- factorial in assembly language.asm
+++ factorial in assembly language.asm
@@ -1,1 +1,34 @@
 ;factorial in assembly language
+
+
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+    NUM DW 5         
+    RESULT DW ?       
+
+.CODE
+MAIN PROC
+   
+    MOV AX, @DATA
+    MOV DS, AX
+
+  
+    MOV AX, 1         
+    MOV CX, 3      
+
+  
+CALC_LOOP:
+    MUL CX            
+   LOOP CALC_LOOP    
+     MOV RESULT, AX 
+    
+   
+   
+   
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+END MAIN
