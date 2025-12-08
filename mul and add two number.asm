f4bb2ae0c285c9bcf5610571220a9e2363cefa87
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2025-12-09 01:49:49 +0600

    Create mul and add two number.asm

---
+++ mul and add two number.asm
@@ -0,0 +1,81 @@
+ include 'emu8086.inc' 
+.model small
+.stack 100h
+.data
+a db ?
+b db ?
+.code
+main proc
+    
+    mov ax,@data
+    mov ds,ax 
+    
+    print "Enter 1st Number:"
+    
+    mov ah,1
+    int 21h
+    sub al,48   ; convert Character to number
+    mov a,al 
+    
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+     int 21h
+    
+   
+   
+   print "Enter 2nd Number:" 
+    mov ah,2
+    
+   mov ah,1
+   int 21h
+   sub al,48
+   mov b,al 
+   
+   
+   
+   
+   
+   
+    mov ah,2
+    mov dl,10
+    int 21h
+    mov dl,13
+    int 21h
+    
+    
+    mov al,a
+    
+  
+   ;add al,b  ;add two number
+   
+    mul al  ;multipication
+   
+   aam
+   mov bx,ax
+   
+   
+   
+   
+    print "the result is:" 
+  
+   
+   mov ah,2
+   mov dl,bh 
+   add dl,48
+   int 21h 
+            
+    mov ah,2
+   mov dl,bl
+     add dl,48 
+   int 21h 
+   
+    
+   
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    main endp
+end main
